adc67be7f19c5da8e83ae400b5d2c2176a8a3c87
Miika-Petteri Matikainen <miikapetteri@gmail.com>
2015-02-08 11:22:01 +0200

    Initial stub for board

---
+++ src/Board.hs
@@ -0,0 +1,22 @@
+module Board (Board) where
+
+import Data.Array
+import Piece
+
+data Square = Square Piece | Empty
+            deriving (Show, Eq)
+
+type Coordinates = (Int, Int)
+type Board = Array Coordinates Square
+
+initialBoard :: Board
+initialBoard = listArray ((0, 0), (3, 3)) (repeat Empty)
+
+movePiece :: Board -> Piece -> Coordinates -> Maybe Board
+movePiece = undefined
+
+isWinningState :: Board -> Bool
+isWinningState = undefined
+
+isDrawState :: Board -> Bool
+isDrawState = undefined
---
+++ src/Piece.hs
@@ -0,0 +1,16 @@
+module Piece (Piece) where
+
+data Color = White | Black
+           deriving (Show, Eq)
+
+data Shape = Circular | Square
+           deriving (Show, Eq)
+
+data Height = Short | Tall
+            deriving (Show, Eq)
+
+data Top = Hollow | Solid
+         deriving (Show, Eq)
+
+data Piece = Piece Color Shape Height Top
+           deriving (Show, Eq)
